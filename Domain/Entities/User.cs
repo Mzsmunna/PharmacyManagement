f461d884588025965bd387d11cfc7115f84c0a24
Mamunuz Zaman <mzs.munna@gmail.com>
2026-02-01 23:34:14 +0600

    data fetch - og

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -9,8 +9,9 @@ namespace Domain.Entities
         public required string Name { get; set; }
         public required string Email { get; set; }
         public required string Phone { get; set; }
-        public required string Role { get; set; }
+        public required string Username { get; set; }
         public required string Password { get; set; }
+        public required string Role { get; set; }
         public bool IsActive { get; set; } = true;
     }
 }
---
+++ WebApi/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Application.Abstractions;
+using Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    //[Authorize(Roles = AppRole.User)]
+    //[Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController(IBaseRepository<User> userSqlRepo) : ControllerBase
+    {
+        //[HttpGet, Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var result = await userSqlRepo.GetAllAsync();
+            return Ok(result);
+        }
+    }
+}
