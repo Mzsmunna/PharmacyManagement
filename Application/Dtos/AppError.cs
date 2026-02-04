5cc2551c0e5b88529ebd6ed9df9e374728654f59
Mamunuz Zaman <mzs.munna@gmail.com>
2026-02-04 19:41:39 +0600

    func adding

--- Application/Dtos/AppError.cs
+++ Application/Dtos/AppError.cs
@@ -14,14 +14,14 @@ namespace Application.Dtos
     )
     {
         public static AppError Ok = new(ErrorType.Ok, string.Empty, string.Empty);
-        public static AppError Missing(string title = "", string message = "", IDictionary<string, string[]>? details = null) => new(ErrorType.Missing, title, message, "204-missing", 204, details);
-        public static AppError Bad(string title = "", string message = "", IDictionary<string, string[]>? details = null) => new(ErrorType.Bad, title, message, "400-bad-request", 400, details);
-        public static AppError Unauthorized(string title = "", string message = "", IDictionary<string, string[]>? details = null) => new(ErrorType.Unauthorized, title, message, "401-unauthorized", 401, details);
-        public static AppError Forbidden(string title = "", string message = "", IDictionary<string, string[]>? details = null) => new(ErrorType.Forbidden, title, message, "403-forbidden", 403, details);
-        public static AppError NotFound(string title = "", string message = "", IDictionary<string, string[]>? details = null) => new(ErrorType.NotFound, title, message, "404-not-found", 404, details);
-        public static AppError Conflict(string title = "", string message = "", IDictionary<string, string[]>? details = null) => new(ErrorType.Conflict, title, message, "409-conflict", 409, details);
-        public static AppError Validation(string title = "", string message = "", IDictionary<string, string[]>? details = null) => new(ErrorType.Validation, title, message, "422-unprocessable-content", 422, details);
-        public static AppError Server(string title = "", string message = "", IDictionary<string, string[]>? details = null) => new(ErrorType.Server, title, message + "; " + Environment.StackTrace, "500-internal-server-error", 500, details);
-        public static AppError Network(string title = "", string message = "", IDictionary<string, string[]>? details = null) => new(ErrorType.Network, title, message, "502-bad-gateway", 502, details);
+        public static AppError Missing(string title = "", string message = "Data for Requested query / payload seems missing", IDictionary<string, string[]>? details = null) => new(ErrorType.Missing, title, message, "204-missing", 204, details);
+        public static AppError Bad(string title = "", string message = "Requested body payload seems empty or corrupted", IDictionary<string, string[]>? details = null) => new(ErrorType.Bad, title, message, "400-bad-request", 400, details);
+        public static AppError Unauthorized(string title = "", string message = "We didn't recognize the user", IDictionary<string, string[]>? details = null) => new(ErrorType.Unauthorized, title, message, "401-unauthorized", 401, details);
+        public static AppError Forbidden(string title = "", string message = "User may not have valid permission", IDictionary<string, string[]>? details = null) => new(ErrorType.Forbidden, title, message, "403-forbidden", 403, details);
+        public static AppError NotFound(string title = "", string message = "Data Not found", IDictionary<string, string[]>? details = null) => new(ErrorType.NotFound, title, message, "404-not-found", 404, details);
+        public static AppError Conflict(string title = "", string message = "Data Maybe already exists", IDictionary<string, string[]>? details = null) => new(ErrorType.Conflict, title, message, "409-conflict", 409, details);
+        public static AppError Validation(string title = "", string message = "Payload seems invalid", IDictionary<string, string[]>? details = null) => new(ErrorType.Validation, title, message, "422-unprocessable-content", 422, details);
+        public static AppError Server(string title = "", string message = "Something went wrong", IDictionary<string, string[]>? details = null) => new(ErrorType.Server, title, message + "; " + Environment.StackTrace, "500-internal-server-error", 500, details);
+        public static AppError Network(string title = "", string message = "Network issue while fetching data", IDictionary<string, string[]>? details = null) => new(ErrorType.Network, title, message, "502-bad-gateway", 502, details);
     }
 }
--- WebApi/Controllers/MedicineBatchesController.cs
+++ WebApi/Controllers/MedicineBatchesController.cs
@@ -1,11 +1,86 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Application.Abstractions;
+using Application.Dtos;
+using Application.Exceptions;
+using Application.Extensions;
+using Application.Payloads;
+using Domain.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class MedicineBatchesController : ControllerBase
+    public class MedicineBatchesController(IBaseRepository<MedicineBatch> repository) : ControllerBase
     {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await repository.GetAllAsNoTrackAsync();
+            //var data = result.ToModelList<MedicineBatchDto, MedicineBatch>();      
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            //MedicineDto? data = null;
+            var result = await repository.GetByIdAsNoTrackAsync(id);
+            if (result == null) throw new AppException(AppError.NotFound(typeof(MedicineBatch).Name + ".NotFound", $"No {typeof(MedicineBatch).Name} available for id: " + id));
+            //data = result.ToModel<MedicineBatchDto, MedicineBatch>();
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetByFilter([FromQuery] FilterPayload payload)
+        {
+            //MedicineDto? data = null;
+            var result = await repository.FindAsNoTrackAsync(x => x.MedicineId == payload.MedicineId || x.Id == payload.Id);
+            if (result == null) throw new AppException(AppError.NotFound(typeof(Medicine).Name + ".NotFound", $"No {typeof(MedicineBatch).Name} available for filter: " + payload.ToQueryString()));
+            //data = result.ToModel<MedicineBatchDto, MedicineBatch>();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(MedicineBatch payload)
+        {
+            if (payload == null) throw new AppException(AppError.Bad(typeof(MedicineBatch).Name + ".BadRequest"));
+            if (string.IsNullOrEmpty(payload.MedicineId)) throw new AppException(AppError.Missing(typeof(MedicineBatch).Name + ".Missing", "MedicineId is seems missing"));
+            if (payload.Quantity <= 0) throw new AppException(AppError.Validation(typeof(MedicineBatch).Name + ".Validation", "Quantity can't be 0"));
+            if (payload.UnitPrice <= 0) throw new AppException(AppError.Validation(typeof(MedicineBatch).Name + ".Validation", "UnitPrice can't be 0"));
+            
+            payload.Id = Guid.CreateVersion7().ToString();
+            var result = await repository.AddAsync(payload);
+            var status = await repository.SaveChangesAsync();
+            return Ok(status);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(MedicineBatch payload)
+        {
+            if (payload == null) throw new AppException(AppError.Bad(typeof(MedicineBatch).Name + ".BadRequest"));
+            var result = await repository.GetByIdAsync(payload.Id);
+            if (result == null) throw new AppException(AppError.NotFound(typeof(MedicineBatch).Name + ".NotFound", $"No {typeof(MedicineBatch).Name} available for id: " + payload.Id));
+            if (string.IsNullOrEmpty(payload.MedicineId)) throw new AppException(AppError.Missing(typeof(MedicineBatch).Name + ".Missing", "MedicineId is seems missing"));
+            if (payload.Quantity <= 0) throw new AppException(AppError.Validation(typeof(MedicineBatch).Name + ".Validation", "Quantity can't be 0"));
+            if (payload.UnitPrice <= 0) throw new AppException(AppError.Validation(typeof(MedicineBatch).Name + ".Validation", "UnitPrice can't be 0"));
+            
+            result.BatchNo = payload.BatchNo;
+            result.MedicineId = payload.MedicineId;
+            result.Quantity = payload.Quantity;
+            result.UnitPrice = payload.UnitPrice;
+            result.Discount = payload.Discount;
+            result.Currency = payload.Currency;
+            result.ExpiryDate = payload.ExpiryDate.ToUniversalTime();
+            await repository.SaveChangesAsync();
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var result = await repository.DeleteByIdAsync(id);
+            return Ok(result);
+        }
     }
 }
