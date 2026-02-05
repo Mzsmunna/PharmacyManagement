f6670481992adf8af84c221d298a58ad86b339a2
Mamunuz Zaman <mzs.munna@gmail.com>
2026-02-05 23:48:39 +0600

    fe - og

---
+++ WebApp/wwwroot/js/med.js
@@ -0,0 +1,29 @@
+﻿const medsForm = document.getElementById("medsForm");
+const apiUrl = "https://localhost:7000/api/Medicines/";
+
+medsForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const medName = document.getElementById("medName").value;
+  const medType = document.getElementById("medType").value;
+  const medDesc = document.getElementById("medDesc").value;
+
+  console.log("medName:", medName);
+  console.log("medType:", medType);
+  console.log("medDesc:", medDesc);
+
+  if (medName && medType && medDesc) {      
+        var payload = {
+            Name: medName,
+            Type: medType,
+            Description: medDesc,
+            Image: "",
+        };
+        const res = await apiRequest("POST", authUrl, payload);
+        if (!res.ok) {
+            alert("Invalid email or password");
+            return;
+        }
+        const data = await res.json();
+  }
+});
